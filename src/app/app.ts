50b2684fca358911968a083cad4efb754c7b6907
andres.fernandezgonzalez.contractor <andres.fernandezgonzalez.contractor@bbva.com>
2026-03-06 13:33:15 +0100

    update images

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, CommonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, FormsModule, MatDatepickerModule,
+    MatInputModule, MatCardModule, MatNativeDateModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/activities/activities.ts
+++ src/app/components/activities/activities.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ChangeDetectionStrategy, Inject, PLATFORM_ID, ChangeDetectorRef, model, HostListener } from '@angular/core';
 
+
+// Importar el DatePipe
+import { DatePipe } from '@angular/common';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+/** @title Datepicker inline calendar example */
 @Component({
-  selector: 'app-activities',
-  imports: [],
-  templateUrl: './activities.html',
-  styleUrl: './activities.css',
+  selector: 'activities',
+  templateUrl: 'activities.html',
+  styleUrls: ['activities.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule, CommonModule, MatNativeDateModule, MatInputModule, MatButtonModule], // Asegúrate de que el módulo esté importado
 })
-export class Activities {
+export class Activities {    
+  isCalendarOpen: boolean = false;  // Para controlar si el calendario está abierto
+  selected = model<Date | null>(null);
+  isBrowser: boolean;
+  selectedEvents: string[] = [];
+  isSmallScreen: boolean = false;   // Variable para detectar pantallas pequeñas
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private cdRef: ChangeDetectorRef) {
+    this.isBrowser = isPlatformBrowser(this.platformId); // Verificamos si estamos en el navegador
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateScreenSize();
+    }
+  }
+
+  // Detecta el tamaño de la pantalla y actualiza la variable isSmallScreen
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateScreenSize();
+  }
+
+  // Función para actualizar el estado del tamaño de la pantalla
+  updateScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 768; // Consideramos pantallas pequeñas si son menos de 768px
+  }
+
+  // Al seleccionar una fecha, cierra el calendario en pantallas pequeñas
+  onDateSelected(date: Date) {
+    this.selected.set(date);
+    if (this.isSmallScreen) {
+      this.isCalendarOpen = false;  // Cierra el calendario en pantallas pequeñas
+    }
+  }
+
+  toggleCalendar() {
+    this.isCalendarOpen = !this.isCalendarOpen;  // Alterna la apertura del calendario
+  }
+  
 }
