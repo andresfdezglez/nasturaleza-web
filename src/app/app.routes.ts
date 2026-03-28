4d291fc01a91e72e1ae65cc379765cc19bb0a150
andres.fernandezgonzalez.contractor <andres.fernandezgonzalez.contractor@bbva.com>
2026-03-28 15:22:53 +0100

    remove reviews component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Activities } from './components/activities/activities';
-import { Reviews } from './components/reviews/reviews';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'activities', component: Activities },
-  { path: 'reviews', component: Reviews }
+  { path: 'activities', component: Activities }
 ];
