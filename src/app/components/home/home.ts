2e5ddf899ea90e4a994e1f51cbdada849742358c
andres.fernandezgonzalez.contractor <andres.fernandezgonzalez.contractor@bbva.com>
2026-03-28 15:23:25 +0100

    update header, about me section and reviews section

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
 import { DatabaseService } from '../../services/database';
@@ -23,9 +23,45 @@ import { DatabaseService } from '../../services/database';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements AfterViewInit {
 
   private router = inject(Router)
+  @ViewChild('seccionSobreMi') seccion!: ElementRef;
+  @ViewChild('carouselTrack', { static: false }) carouselTrack?: ElementRef<HTMLElement>;
+
+  // Signal para activar la animación
+  isVisible = signal(false);
+  private platformId = inject(PLATFORM_ID);
+
+  ngAfterViewInit() {
+    // Solo ejecutamos si estamos en el navegador
+    if (isPlatformBrowser(this.platformId)) {
+
+      const opciones = {
+        root: null, // relativo al viewport (pantalla)
+        rootMargin: '0px',
+        threshold: 0.05 // Se activa cuando el 20% de la sección asoma
+      };
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          // Log para depuración: Abre la consola (F12) para ver esto
+          console.log('¿Sección visible?:', entry.isIntersecting);
+
+          if (entry.isIntersecting) {
+            this.isVisible.set(true); // Cambiamos el signal a true
+
+            // Una vez animado, dejamos de observar para ahorrar recursos
+            observer.unobserve(this.seccion.nativeElement);
+          }
+        });
+      }, opciones);
+
+      if (this.seccion) {
+        observer.observe(this.seccion.nativeElement);
+      }
+    }
+  }
 
   actividades = [
     {
@@ -52,7 +88,7 @@ export class Home {
   isMobile = signal<boolean>(false);
   listaReviews = signal<any[]>([]);
 
-  constructor(private supabaseService: DatabaseService){}
+  constructor(private supabaseService: DatabaseService) { }
 
   ngOnInit(): void {
     // Escuchamos el cambio de tamaño de pantalla
@@ -78,4 +114,22 @@ export class Home {
       console.error("Error cargando reviews:", error);
     }
   }
+
+scroll(direction: 'left' | 'right') {
+  console.log("HOLA");
+  
+  // Si por lo que sea no detecta el track, salimos de la función sin error
+  if (!this.carouselTrack || !this.carouselTrack.nativeElement) {
+    console.warn("El track aún no está disponible");
+    return;
+  }
+
+  const track = this.carouselTrack.nativeElement;
+  const scrollAmount = track.offsetWidth * 0.8;
+
+  track.scrollBy({
+    left: direction === 'left' ? -scrollAmount : scrollAmount,
+    behavior: 'smooth'
+  });
+}
 }
