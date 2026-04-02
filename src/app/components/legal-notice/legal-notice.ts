70b3726c1b13975bb6ea4efaba8c7f3523f48a02
andres.fernandezgonzalez.contractor <andres.fernandezgonzalez.contractor@bbva.com>
2026-04-02 21:55:00 +0200

    scroll to top for legal notice

--- src/app/components/legal-notice/legal-notice.ts
+++ src/app/components/legal-notice/legal-notice.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-legal-notice',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.css',
 })
-export class LegalNotice {
+export class LegalNotice implements OnInit{
+
+    private platformId = inject(PLATFORM_ID);
+    
+    ngOnInit(): void {
+      this.scrollToTop();
+    }
+
+    private scrollToTop() {
+      if (isPlatformBrowser(this.platformId)) {
+        window.scrollTo(0, 0);
+        const mainContent = document.querySelector('.mat-sidenav-content');
+        if (mainContent) mainContent.scrollTop = 0;
+      }
+    }
 
 }
